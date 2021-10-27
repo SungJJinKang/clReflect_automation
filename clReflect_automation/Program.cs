1f87615ed745f4b6184e21aaa688fc0b849a255f
sungJinKangdev <hour30000@gmail.com>
2021-10-27 18:10:24 +0900

    recreate project file

---
+++ clReflect_automation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace clReflect_automation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
