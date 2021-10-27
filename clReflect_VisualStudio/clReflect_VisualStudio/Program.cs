645641d277d01f8d5eaddb05fd69293fad263c9d
SungJinKang <hour30000@gmail.com>
2021-10-27 17:01:39 +0900

    implementing parsing vcxproj with regex

---
+++ clReflect_VisualStudio/clReflect_VisualStudio/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace clReflect_VisualStudio
+{
+    class Program
+    {
+
+        static List<string> GetAdditionalDirectories
+            (
+            in string vcxprojPath,
+            in string Configuration, 
+            in string Platform
+            )
+        {
+            string text = System.IO.File.ReadAllText(vcxprojPath);
+
+            string pattern = @"<ItemDefinitionGroup(.*?)\/ItemDefinitionGroup>";
+            Regex rgx = new Regex(pattern, RegexOptions.Singleline);
+            MatchCollection matches = rgx.Matches(text);
+
+            foreach(Match match in matches)
+            {
+                string itemDefGroup = match.Value;
+            }
+
+            List<string> additionalDirectories = new List<string>();
+
+            return additionalDirectories;
+        }
+
+        static void Main(string[] args)
+        {
+            GetAdditionalDirectories("C:/Voxel_Doom3_From_Scratch/Doom3/Doom3.vcxproj", "", "");
+        }
+    }
+}
